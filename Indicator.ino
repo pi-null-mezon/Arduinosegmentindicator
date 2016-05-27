472c182e94535ed8266b408642c26844c0e1a61b
pi-null-mezon <pi-null-mezon@yandex.ru>
2016-05-27 22:59:41 +0300

    two sensor types

--- Indicator.ino
+++ Indicator.ino
@@ -1,13 +1,15 @@
-/* DHT library courtesy provided by: 
- * https://github.com/adafruit/DHT-sensor-library
- */
 #include <DHT.h>
+#include <OneWire.h>
 
-//Sensor---------------------------------------------------
+//DHT22---------------------------------------------------
 #define DHTPIN A5     // what digital pin we're connected to
 #define DHTTYPE DHT22 // AM2302 is the same but from the other manufacturer
 #define MIN_MEASURE_DELAY 5000 // it is not a milliseconds, just a cycles number
 DHT dht(DHTPIN, DHTTYPE);
+//DS18B20-------------------------------------------------
+#define MEASURE_RESOLUTION 0.0625
+#define ONEWIREPIN 2
+OneWire ds(ONEWIREPIN);
 
 //Indicator------------------------------------------------
 #define DOT_FLAG        0x01 // indicate a dot with symbol
@@ -33,6 +35,7 @@ const byte v_symbolCode[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7
 #define SYMBOL_T_FLAG 0x10
   
 //Miscellaneous--------------------------------------------------
+#define MAX_SENSORS_COUNT 8
 #define MEASUREMENTS_REGIME 0x01
 #define ANALOGREAD_REGIME 0x02
 #define ANALOG_PIN A6
@@ -43,28 +46,27 @@ float     v_measure[2];
 float     measure;
 int       analogvalue;
 byte      regime = MEASUREMENTS_REGIME;
-byte      valuetypeFlag; 
+byte      valuetypeFlag;
+byte      v_addr[8 * MAX_SENSORS_COUNT];
+byte      addr[8];
+byte      m_sensors; 
 //-------------------------------------------------------------------------
 
 void setup() {
   initIndicator();
   dht.begin();
-  // button initialization 
-  //pinMode(BUTTON_PIN, INPUT_PULLUP);
-  //attachInterrupt(0, buttonPressed, FALLING); // 0 interrupt for Arduino Uno linked with 2 GIO 
+  initOneWireLine();
 }
 
 //-------------------------------------------------------------------------
 void loop() {
   switch(regime) {  
     case MEASUREMENTS_REGIME:
-      for(byte k = 0 ; k < 2; k++) {
-        if(k == 0)
-            valuetypeFlag = SYMBOL_H_FLAG;
-          else
-            valuetypeFlag = SYMBOL_T_FLAG;
-        measure = v_measure[k];        
-        /*if(measure < 0.0) {
+      for(byte n =0; n < m_sensors; n++) {
+        for(byte i = 0; i < 8; i++)
+            addr[i] = v_addr[i + n*8];
+        measure = readDS18B20(addr);
+        if(measure < 0.0) {
           measure = - measure;
           v_symbols[0] = (byte)(measure*10) % 10;
           v_symbols[1] = (byte)measure % 10;
@@ -89,7 +91,43 @@ void loop() {
                 setSymbol(2, i > 0 ? 0:v_symbols[2], ((i < 1) && (v_symbols[2] == 0)) ? 0 : BLANKZERO_FLAG);
                 setSymbol(3, 0, BLANKZERO_FLAG);
             }   
-        } else {*/
+        } else {
+          v_symbols[0] = (byte)(measure*10) % 10;
+          v_symbols[1] = (byte)measure % 10;
+          v_symbols[2] = (byte)(measure/10) % 10;
+          v_symbols[3] = (byte)(measure/100) % 10;
+          for(byte i = 0; i < 4; i++)
+            for(uint16_t j = 0; j < (measureDelay >> 3); j++) {
+                setSymbol(0, v_symbols[0], 0);
+                setSymbol(1, i > 0 ? v_symbols[1]:0, ((i > 0) && (v_symbols[1] == 0)) ? 0 : BLANKZERO_FLAG);
+                setSymbol(2, i > 1 ? v_symbols[2]:0, ((i > 1) && (v_symbols[2] == 0)) ? 0 : BLANKZERO_FLAG);
+                setSymbol(3, i > 2 ? v_symbols[3]:0, ((i > 2) && (v_symbols[3] == 0)) ? 0 : BLANKZERO_FLAG);
+            }
+          for(uint16_t j = 0; j < measureDelay; j++) {
+              setSymbol(0, v_symbols[0], 0);
+              setSymbol(1, v_symbols[1], DOT_FLAG);
+              setSymbol(2, v_symbols[2], 0);
+              setSymbol(3, v_symbols[3], BLANKZERO_FLAG);
+          } 
+          for(byte i = 0; i < 4; i++)
+            for(uint16_t j = 0; j < (measureDelay >> 3); j++) {
+                setSymbol(0, i > 2 ? 0:v_symbols[0], ((i < 3) && (v_symbols[0] == 0)) ? 0 : BLANKZERO_FLAG);
+                setSymbol(1, i > 1 ? 0:v_symbols[1], ((i < 2) && (v_symbols[1] == 0)) ? 0 : BLANKZERO_FLAG);
+                setSymbol(2, i > 0 ? 0:v_symbols[2], ((i < 1) && (v_symbols[2] == 0)) ? 0 : BLANKZERO_FLAG);
+                setSymbol(3, 0, BLANKZERO_FLAG);
+            }
+        }  
+        startConversionDS18B20(addr); 
+        //analogvalue = analogRead(ANALOG_PIN); 
+        //measureDelay = MIN_MEASURE_DELAY + (analogvalue << 4);
+      }
+     
+      for(byte k = 0 ; k < 2; k++) {
+        if(k == 0)
+            valuetypeFlag = SYMBOL_H_FLAG;
+        else
+            valuetypeFlag = SYMBOL_T_FLAG;
+        measure = v_measure[k];        
           v_symbols[0] = (byte)(measure*10) % 10;
           v_symbols[1] = (byte)measure % 10;
           v_symbols[2] = (byte)(measure/10) % 10;         
@@ -115,10 +153,11 @@ void loop() {
             }
         //}
       }
-      readSensor();     
+      readDHT22();     
       //analogvalue = analogRead(ANALOG_PIN); 
       //measureDelay = MIN_MEASURE_DELAY + (analogvalue << 4);     
-      break;      
+      break;
+            
     case ANALOGREAD_REGIME:
       v_symbols[0] = (byte)(analogvalue % 10);
       v_symbols[1] = (byte)((analogvalue/10) % 10);
@@ -202,7 +241,7 @@ void setSymbol(byte digit, byte value, byte flags) // use v_symbolCode vector fo
 
 //-------------------------------------------------------------------------
 
-void readSensor()
+void readDHT22()
 {
   noInterrupts();
   v_measure[0] = dht.readHumidity();
@@ -231,3 +270,44 @@ void buttonPressed()
   }
 }
 //----------------------------------------------------------------------
+
+void initOneWireLine()
+{
+  m_sensors = 0;
+  byte addr[8];
+  while(ds.search(addr)) {
+    for(byte i = 0; i < 8; i++)
+      v_addr[i + 8 * m_sensors] = addr[i];
+    if(m_sensors < MAX_SENSORS_COUNT)
+      m_sensors++;     
+  }
+}
+
+//-------------------------------------------------------------------------
+
+void startConversionDS18B20(byte *addres)
+{
+  noInterrupts();
+  ds.reset();
+  ds.select(addres);
+  ds.write(0x44, 1); // start conversion, with parasite power on at the end
+  interrupts();  
+}
+
+//-------------------------------------------------------------------------
+
+float readDS18B20(byte *addres)
+{
+  noInterrupts();
+  ds.reset();
+  ds.select(addres);    
+  ds.write(0xBE); // start read scratchpad  
+  byte lsb = ds.read();  
+  byte msb = ds.read();
+  byte sign = msb & 0xF0;
+  interrupts();
+  if(sign == 0xF0)
+    return -((int)((byte)~lsb) | ((int)((byte)~msb) << 8)) * MEASURE_RESOLUTION;
+  else
+    return ((int)lsb | ((int)msb << 8)) * MEASURE_RESOLUTION;      
+}
