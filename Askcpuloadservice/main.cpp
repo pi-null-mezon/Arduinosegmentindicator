21817ec6d7d80df41c6bf8323a43f360dfc2beef
Alex Taranov <pi-null-mezon@yandex.ru>
2017-07-07 23:39:13 +0300

    update

--- Askcpuloadservice/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "qhardwarestatusservice.h"
-#include "qhardwarestatusexplorer.h"
-
-int main(int argc, char *argv[])
-{
-    QHardwarestatusservice _service(argc, argv);
-    return _service.exec();
-}
